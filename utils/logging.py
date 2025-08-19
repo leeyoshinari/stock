49d90591f427790977144583704e70bbb236f082
风儿 <ljc@fengerdeMacBook-Pro.local>
2025-08-19 23:12:45 +0800

    add

--- utils/logging.py
+++ utils/logging.py
@@ -26,6 +26,6 @@ logger.setLevel(level=log_level.get(LOGGER_LEVEL))
 file_handler = logging.handlers.TimedRotatingFileHandler(
     os.path.join(log_path, 'run.log'), when='midnight', interval=1, backupCount=15)
 file_handler.suffix = '%Y-%m-%d.log'
-file_handler = logging.StreamHandler()
+# file_handler = logging.StreamHandler()
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
